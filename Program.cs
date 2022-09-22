640aa8ca77b37baf08c789967e48dbb9af4bec02
marco aurélio <marcoaurelio250303@gmail.com>
2022-09-22 19:59:02 -0300

    nova implemtaçaõ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor");
+        var valor = int.Parse(Console.ReadLine());
+        var total = valor * valor;
+        Console.WriteLine($" o valor {valor} multiplicado por ele mesmo é " + total);
+    }
+}
